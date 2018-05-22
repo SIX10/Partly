a42ffb222796df19f6e42a3d28ee71db1e27eada
SIX10 <six10@protonmail.com>
2018-05-21 21:53:46 -0500

    Migrated to discord.py 1.00 and added quit
    thanks to discord.py discord for helping with my retardation

--- partly.py
+++ partly.py
@@ -14,67 +14,75 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print("-----------")
-    await bot.change_presence(game=discord.Game(name="yo girl | +commands "))
+    await bot.change_presence(activity=discord.Game(name="with yo girl | +commands "))
 
 
 
 # Math
 @bot.command()
-async def add(a: int, b: int):
-    await bot.say(a+b)
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
 
 @bot.command()
-async def multiply(a: int, b: int):
-    await bot.say(a*b)
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
 
 @bot.command()
-async def divide(a: int, b: int):
+async def divide(ctx, a: int, b: int):
     if b == 0:
-        await bot.say("Cannot divide by zero.")
+        await ctx.send("Cannot divide by zero.")
     else:
-        await bot.say(a/b)
+        await ctx.send(a/b)
 
 @bot.command()
-async def subtract(a: int, b: int):
-    await bot.say(a-b)
+async def subtract(ctx, a: int, b: int):
+    await ctx.send(a-b)
 
 #Fun
 @bot.command()
-async def troll():
+async def troll(ctx):
     async with aiohttp.ClientSession() as session:
         async with session.get("http://rolloffle.churchburning.org/troll_me_text.php") as site:
             if site.status == 200:
-                await bot.say(await site.text())
+                await ctx.send(await site.text())
             else:
-                await bot.say("Could not connect.")
+                await ctx.send("Could not connect.")
 
 @bot.command()
-async def godsays():
+async def godsays(ctx):
     async with aiohttp.ClientSession() as session:
         async with session.get("https://templeos.net/") as site:
             if site.status == 200:
-                await bot.say(await site.text())
+                await ctx.send(await site.text())
             else:
-                await bot.say("Could not connect to heaven.")
+                await ctx.send("Could not connect to heaven.")
 
 @bot.command()
-async def brap():
-    await bot.say("BRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP")
+async def brap(ctx):
+    await ctx.send("BRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP")
+
 
 #Misc
 @bot.command()
-async def invite():
+async def invite(ctx):
     msg = discord.Embed(title="Invite",
                         description="You can invite me to your server with https://discordapp.com/oauth2/authorize?client_id=447940688508878868&scope=bot&permissions=0",
                         color=discord.Color.red())
-    await bot.say(embed=msg)
+    await ctx.send(embed=msg)
 
 
 @bot.command()
-async def commands():
+async def commands(ctx):
     msg = discord.Embed(title="Commands",
                         description="Partly's commands are found at https://github.com/SIX10/Partly/blob/master/Help.md",
                         color=discord.Color.red())
-    await bot.say(embed=msg)
+    await ctx.send(embed=msg)
+
+@bot.command()
+async def quit(ctx):
+    if ctx.author.id == 130853292275269632:
+        await ctx.send("Shutting down...")
+        await bot.logout()
+    else: await ctx.send("You aren't my owner!")
 
 bot.run(token)
