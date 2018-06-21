9a1ff277ea337415c2dc437ea621935740e064e4
SIX10 <six10@protonmail.com>
2018-06-21 08:28:41 -0500

    Removed "fucken"

--- shitposts.py
+++ shitposts.py
@@ -15,10 +15,6 @@ class Shitposts:
         await ctx.send("Jenkins is one big ole poo poo head.")
 
     @commands.command()
-    async def fucken(self, ctx):
-        await ctx.send("Ay you im gonna fuckin NAYNAY on this bitch fuck nigga as jew OK dont fucking mess with me or you will fucking regret it bucko you fuckin sauck my pp right nowe or face the concequences kid ok you follow my rules or you follow the road ok no fucking stick a straw up my ass aand suck and suck as hard and long as you can or im going to MMMMHMMMMMMMmmm praAISe the lord you ok now fucking get your Stoopid ass bitch ass face right here right now before i OOOAHHHHyaaa it mhmmm that was the LAST STRAW before I NUT On your face LIKE i did to jess down the street mmhmmm baby!")
-
-    @commands.command()
     async def daddy(self, ctx):
         await ctx.send("SIX10#0877 is my daddy")
 
