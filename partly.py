85e9516aaeec50abfd0bf9bbd2267ee5201e8e13
SIX10 <six10@protonmail.com>
2018-10-29 19:41:40 -0500

    Redid p+ping

--- partly.py
+++ partly.py
@@ -12,15 +12,16 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-token= ""
+token= "NDQ3OTQwNjg4NTA4ODc4ODY4.DfapCw.eE4B4iEftfQisVZzpTtajZWIbMs"
 
 if not discord.opus.is_loaded():
     discord.opus.load_opus()
 
 cogs = ['cogs.music',
-        'cogs.fun',,
+        'cogs.fun',
         'cogs.media',
-        'cogs.shitposts']
+        'cogs.shitposts',
+        'cogs.4chanMarkov.py']
 
 bot = commands.Bot(command_prefix="p+", description="A bot that does stuff. By SIX10#0877.")
 @bot.event
@@ -29,34 +30,38 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print("-----------")
-    await bot.change_presence(activity=discord.Game(name="with yo girl | p+help "))
+    await bot.change_presence(activity=discord.Game(name="with yo girl | p+commands "))
 
     for cog in cogs:
         bot.load_extension(cog)
 
 @bot.command()
 async def invite(ctx):
-    """Invite Partly to your server!"""
     msg = discord.Embed(title="Invite",
                         description="You can invite me to your server with https://discordapp.com/oauth2/authorize?client_id=447940688508878868&scope=bot&permissions=0",
                         color=discord.Color.red())
     await ctx.send(embed=msg)
 
+
+@bot.command()
+async def commands(ctx):
+    msg = discord.Embed(title="Commands",
+                        description="Partly's commands are found at https://github.com/SIX10/Partly/blob/master/Help.md",
+                        color=discord.Color.red())
+    await ctx.send(embed=msg)
+
 @bot.command()
 async def servers(ctx):
-    """Shows how many servers Partly is connected to"""
     msg = "Connected to " + str(len(bot.guilds)) + " servers"
     await ctx.send(msg)
 
 @bot.command()
 async def users(ctx):
-    """Shows how many users Partly is serving"""
     msg = "Serving " + str(len(bot.users)) + " users"
     await ctx.send(msg)
 
 @bot.command()
-async def home(ctx):
-    """Invite to Partly's Discord Server!"""
+async def bug(ctx):
     await ctx.send("Found a bug? Report it to https://discord.gg/7KWqhzb or message SIX10#0877")
 
 @bot.command(hidden=True)
@@ -73,7 +78,7 @@ async def quit(ctx):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("Pong")
+    await ctx.send(f"Pong! Responded in {round(bot.latency*1000, 2)} ms")
 
 @bot.command(name='reload', hidden=True)
 async def cog_reload(self, ctx, *, cog: str):
@@ -85,5 +90,4 @@ async def cog_reload(self, ctx, *, cog: str):
             await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
             await ctx.send('**`SUCCESS`**')
-
 bot.run(token)
